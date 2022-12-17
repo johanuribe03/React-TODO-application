0461040999569a814d1fa4999563e5cd5aa2d252
johanuribe03 <johan.uribe@gmail.com>
2022-12-17 19:15:38 +0000

    adding elements with new input

--- src/js/component/todo.jsx
+++ src/js/component/todo.jsx
@@ -13,8 +13,18 @@ const ToDo = () => {
             </style>
 
             <h1>todos</h1>
-            <input type="text" onChange={e => setInput(e.target.value)} value={input} />
+            <input id="input" type="text" onChange={e => setInput(e.target.value)} value={input} />
             {/* <input type="text" onChange={e => setInputValue(e.target.value)} value={inputValue} /> */}
+            <li>{document.querySelector('input').value}</li>
+            
+            {/* { const createNode = (el) => { 
+                return document.createElement(el)
+              }
+              const input = document.querySelector('input');
+              console.log(input);
+              let li = createNode('li');
+              li.innerHTML = input.value;
+            } */}
         </div>
     );
 
