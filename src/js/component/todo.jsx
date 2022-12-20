0049e8dc4d54159ba031ea3b6f7d84dc290fcdb3
johanuribe03 <johan.uribe@gmail.com>
2022-12-20 19:43:30 +0000

    Debugging

--- src/js/component/todo.jsx
+++ src/js/component/todo.jsx
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const ToDo = () => {
-    const [inputValue, setInputValue] = useState('');
+    const [inputValue, setInputValue] = useState("");
     const [todos, setTodos] = useState([]);
 
     // Add into array -> concat
@@ -10,34 +10,48 @@ const ToDo = () => {
 
     return (
         <div>
-            <style jsx global>{`
+            {/* <style jsx global>{`
                 h1 {
                     font-size: 90px;
                 }
             `}
-            </style>
+            </style> */}
 
-            <h1>todos</h1>
-            <input 
-                id="input" 
-                type="text" 
-                onChange={(e) => {
-                    return setInputValue(e.target.value);
-                }} 
-                value={inputValue}
-                onKeyPress={(e) => {
-                    e.key === "Enter" ? 
-                    setTodos(todos.concat(inputValue)) ;
-                    setInputValue("")
-                }
-                placeholder="What do you need to do"
-            />
-
-            {todos.map((t) => { //for each todo i want an li
+            <h1>todos {inputValue}</h1>
+            <ul>
                 <li>
-                    {t} <i class="fas fa-trash-alt"></i>
+                    <input  
+                        type="text" 
+                        onChange={(e) => 
+                            setInputValue(e.target.value)
+                        }
+                        value={inputValue}
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                                console.log("Enter HOla");
+                                setTodos(todos.concat(inputValue));
+                                setInputValue("");
+                                console.log(inputValue);
+                                console.log(todos);
+
+                                console.log(setInputValue);
+                            }
+                            // e.key === "Enter" ? 
+                            // setTodos(todos.concat(inputValue)) ;
+                            // setInputValue("")
+                        
+                        } }
+                        placeholder="What do you need to do?"
+                    ></input>
                 </li>
-            })}
+
+                {todos.map((t) => { //for each todo i want an li
+                    <li>
+                        hola 
+                        {/* <i class="fas fa-trash-alt"></i> */}
+                    </li>
+                })}
+            </ul>
 
             {/* <input type="text" onChange={e => setInputValue(e.target.value)} value={inputValue} /> */}
                 {/* <li>{document.querySelector('input').value}</li> */}
